a9ad64f89f781a40cfacc23c04cf88cc6f1d6d6c
liguobao <codelover@qq.com>
2023-12-08 15:51:51 +0800

    fix: ans word cloud

--- ans_word_cloud_main.py
+++ ans_word_cloud_main.py
@@ -9,7 +9,7 @@ from ans_word_cloud_all import to_word_cloud_img
 if __name__ == "__main__":
     parse = argparse.ArgumentParser(prog='zhihu_answer_donwload')
     parse.add_argument(
-        '-id', '--ans', help='question_id')
+        '-id', '--question_id', help='question_id')
     cmd_args = parse.parse_args()
     if not path.exists("./data"):
         os.mkdir("./data")
